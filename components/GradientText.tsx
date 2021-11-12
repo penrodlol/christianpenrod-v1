719f9505942e2adf642f48b4873be71c05567ac2
penrodlol <christian@penrod.contact>
2021-11-11 20:15:08 -0500

    feat: gradient text component

---
+++ components/GradientText.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: -webkit-linear-gradient(45deg, var(--accent), #cab4e6);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+export const GradientText = ({ children }: PropsWithChildren<{}>) => (
+  <Wrapper>{children}</Wrapper>
+);
