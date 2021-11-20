eb53d117b8c06e446fc402966b11b9f19c43b1be
penrodlol <christian@penrod.contact>
2021-11-20 14:16:15 -0500

    feature: header menu * Route minor styling. * gsap stagger routes in/out of view.

--- components/Header/HeaderMenu.tsx
+++ components/Header/HeaderMenu.tsx
@@ -1,6 +1,7 @@
 import { Hamburger } from '@Components/Hamburger';
 import { Overlay } from '@Components/Overlay';
-import { PropsWithChildren, useState } from 'react';
+import gsap from 'gsap';
+import { PropsWithChildren, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const HamburgerWrapper = styled.div`
@@ -9,12 +10,18 @@ const HamburgerWrapper = styled.div`
 `;
 
 const Route = styled.a`
-  font-size: 40px;
+  font-size: 50px;
   cursor: pointer;
   display: block;
   width: max-content;
-  padding: 1rem;
+  margin: 2rem 0;
   font-weight: 600;
+  border-radius: 0.1rem;
+  transform: translateX(-15rem);
+
+  &:first-child {
+    margin-top: 4rem;
+  }
 `;
 
 export interface HeaderMenuProps {
@@ -22,16 +29,40 @@ export interface HeaderMenuProps {
 }
 
 export const HeaderMenu = (props: PropsWithChildren<HeaderMenuProps>) => {
-  const [show, showMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const routeRefs = useRef<Array<HTMLAnchorElement>>([]);
+
+  function toggleMenu() {
+    gsap.to(routeRefs.current, {
+      onStart: () => setMenu(true),
+      onComplete: () => {
+        if (menu) gsap.delayedCall(0.1, () => setMenu(false));
+      },
+      opacity: menu ? 0 : 1,
+      x: menu ? '-13rem' : '2.5rem',
+      ease: `back.${menu ? 'in' : 'out'}(2)`,
+      stagger: {
+        each: 0.1,
+        from: menu ? 1 : 0,
+      },
+      duration: 0.3,
+    });
+  }
 
   return (
     <div>
-      <HamburgerWrapper onClick={() => showMenu(!show)}>
+      <HamburgerWrapper onClick={toggleMenu}>
         <Hamburger type="button" aria-label="Toggle navigation menu" />
       </HamburgerWrapper>
-      <Overlay show={show}>
-        {props.routes.map((route) => (
-          <Route key={route}>{route}</Route>
+      <Overlay show={menu}>
+        {props.routes.map((route, i) => (
+          <Route
+            key={route}
+            tabIndex={0}
+            ref={(el: HTMLAnchorElement) => (routeRefs.current[i] = el)}
+          >
+            {route}
+          </Route>
         ))}
       </Overlay>
     </div>
