31cf86c7cb252784f0e4a9b7b7b5f5e94b5ec47f
penrodlol <penrod.christian7984@gmail.com>
2021-11-20 22:59:31 -0500

    chore: register MorphSVGPlugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
+import gsap from 'gsap';
+import { MorphSVGPlugin } from 'gsap/dist/MorphSVGPlugin';
 import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
+
+gsap.registerPlugin(MorphSVGPlugin);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
