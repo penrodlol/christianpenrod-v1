8bbce2aa202d4b2958981abc3ffaf03766f998f2
penrodlol <penrod.christian7984@gmail.com>
2021-11-20 20:01:54 -0500

    feature: svg * Allow forward ref to parent.

--- components/Svg/Svg.tsx
+++ components/Svg/Svg.tsx
@@ -1,19 +1,30 @@
-import { PropsWithChildren, SVGAttributes } from 'react';
+import {
+  ForwardedRef,
+  forwardRef,
+  PropsWithChildren,
+  SVGAttributes,
+} from 'react';
 import { SvgName, SVGS } from './svgs';
 
 export interface SvgProps extends SVGAttributes<SVGElement> {
   name: SvgName;
 }
 
-export const Svg = (props: PropsWithChildren<SvgProps>) => (
+const SvgComponent = (
+  props: PropsWithChildren<SvgProps>,
+  ref: ForwardedRef<SVGSVGElement>,
+) => (
   <svg
     viewBox="0 0 20 20"
     xmlns="http://www.w3.org/2000/svg"
     width="35"
     height="35"
+    ref={ref}
     {...props}
   >
     <path {...SVGS.get(props.name)} />
     <title>{props.name}</title>
   </svg>
 );
+
+export const Svg = forwardRef(SvgComponent);
