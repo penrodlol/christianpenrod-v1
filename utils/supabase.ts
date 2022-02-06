79c807f1b48596df9ef29153226422ad58ce9e31
penrodlol <christian@penrod.contact>
2022-02-05 21:15:14 -0500

    💄 divide scss for elements

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  `https://${process.env.NEXT_PUBLIC_SUPABASE_DOMAIN}`,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 );
