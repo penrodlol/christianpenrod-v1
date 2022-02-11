a44b3292714ab553c70131f62fad5d50dc0664dd
penrodlol <penrod.christian7984@gmail.com>
2022-02-10 22:47:38 -0500

    ✨ Divider props

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,10 +1,17 @@
-import styled from 'styled-components';
+import { FC } from 'react';
+import styled, { css } from 'styled-components';
 
-const Wrapper = styled.div`
-  background-color: var(--brand-1);
-  border-radius: var(--radius-2);
-  height: var(--size-1);
-  margin: var(--size-3) 0;
-`;
+const Wrapper = styled.div<DividerProps>(
+  ({ size }) => css`
+    background-color: var(--brand-1);
+    border-radius: var(--radius-2);
+    height: var(--size-1);
+    margin: var(--size-${size || '3'}) 0;
+  `,
+);
 
-export const Divider = () => <Wrapper />;
+export interface DividerProps {
+  size?: number;
+}
+
+export const Divider: FC<DividerProps> = (props) => <Wrapper {...props} />;
