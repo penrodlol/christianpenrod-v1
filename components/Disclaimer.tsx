28645e2183b8a3425b8e740054cf43f77f86ec62
penrodlol <christian@penrod.contact>
2022-04-16 14:02:38 -0400

    💄 Removing warning icon from disclaimer

--- components/Disclaimer.tsx
+++ components/Disclaimer.tsx
@@ -1,4 +1,3 @@
-import Warning from '@svg/warning.svg';
 import { FC } from 'react';
 import styled from 'styled-components';
 
@@ -12,24 +11,18 @@ const Wrapper = styled.div`
   font-size: var(--_disclaimer-font-size);
   letter-spacing: var(--font-letterspacing-2);
   padding: var(--size-4);
-
-  svg {
-    transform: translateY(0.2rem);
-    fill: var(--text-2) !important;
-  }
 `;
 
 const InnerWrapper = styled.div`
-  display: grid;
-  grid-template-columns: max-content 1fr;
-  gap: var(--size-2);
+  [class*='CodeSnippet__Wrapper'] {
+    margin: 0;
+  }
 `;
 
 export const Disclaimer: FC = ({ children }) => {
   return (
     <Wrapper>
       <InnerWrapper>
-        <Warning height={25} width={25} />
         <div>{children}</div>
       </InnerWrapper>
     </Wrapper>
